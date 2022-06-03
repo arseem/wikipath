2b77f625829e7f954755c541a956cebcd6f46010
arseem <106632663+arseem@users.noreply.github.com>
2022-06-03 12:45:37 +0200

    multithreading added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
+NUM_THREADS = 100
 class PathPuller:
 
     def __init__(self, start_page, end_page, max_depth=5):
@@ -18,32 +20,38 @@ class PathPuller:
         soup = BeautifulSoup(html, 'html.parser')
         article = soup.find('div', {"id": "bodyContent"})
 
+        self.visited.append(page.split('/wiki/')[-1])
+
         links = []
         for anchor in article.find_all('a'):
             link = anchor.get('href', '/')
             links.append(f'https://wikipedia.org{link}') if link.startswith('/wiki/') and not ':' in link and link.split('/wiki/')[-1] not in self.visited else None
             if link == self.destination:
-                self.current_path.append(link.split('/wiki/')[-1])
-                return -1
+                return -1, link
 
-        return links
+        return page, links
 
     def search(self):
-        links = [self.get_links_from_page(self.current)]
+        links = [[self.get_links_from_page(self.current)]]
         if links==-1:
             return self.current_path
 
         for i in range(self.max_depth):
             links.append([])
-            for l in links[i]:
-                self.visited.append(l.split('/wiki/')[-1])
-                new_links = self.get_links_from_page(l)
-                if new_links == -1:
-                    return self.current_path
-                
-                links[i+1].append(new_links)
+            set_of_links = links[i]
+            with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+                for new_links in executor.map(self.get_links_from_page, [l[-1] for l in set_of_links][0]):
+                    if new_links[0] == -1:
+                        self.current_path.append(set_of_links[0].split('/wiki/')[-1]) if set_of_links[0].split('/wiki/')[-1]!=self.current_path[0] else None
+                        self.current_path.append(new_links[0].split('/wiki/')[-1])
+                        self.current_path.append(new_links[-1].split('/wiki/')[-1])
+                        return self.current_path
+                    
+                    links[i+1].append(new_links)
 
         return 'No connection in specified range'
 
 
+bs = PathPuller('https://wikipedia.org/wiki/Spoon', 'https://wikipedia.org/wiki/Adolf_Hitler')
+print(bs.search())
 
