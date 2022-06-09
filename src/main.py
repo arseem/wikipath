aed43e373d5bf944d9123956b3ed085f6081d7d9
arseem <106632663+arseem@users.noreply.github.com>
2022-06-09 03:06:44 +0200

    update interface

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
@@ -14,18 +15,21 @@ class PathPuller:
         self.current = start_page
         self.destination = end_page
         self.visited = []
-        self.current_path =  [self.current.split('/wiki/')[-1]]
+        self.current_path =  []
 
         self.depth = 0
         self.max_depth = max_depth
 
         self.found = 0
+        self.con_err = []
+        self.json_err = 0
 
 
     def get_links_from_page(self, page):
         try:
             html = requests.get(page).text
         except requests.exceptions.ConnectionError:
+            self.con_err.append(page.split('/wiki/')[-1])
             return page, []
 
         soup = BeautifulSoup(html, 'html.parser')
@@ -42,7 +46,7 @@ class PathPuller:
                 if link.split('/wiki/')[-1] == self.destination.split('/wiki/')[-1]:
                     new_links = (-1, page, link)
                     set_of_links = self.set_of_links
-                    self.current_path.append(set_of_links[0][0].split('/wiki/')[-1]) if set_of_links[0][0].split('/wiki/')[-1]!=self.current_path[0] else None
+                    self.current_path.append(set_of_links[0][0].split('/wiki/')[-1])
                     self.current_path.append(new_links[1].split('/wiki/')[-1])
                     self.current_path.append(new_links[-1].split('/wiki/')[-1])
                     self.current_path = [n.replace('_', ' ').replace('%27', "'") for n in self.current_path]
@@ -55,6 +59,7 @@ class PathPuller:
     def search(self):
         links = [[self.get_links_from_page(self.current)]]
         if links[0][0][0]==-1:
+            self.current_path.append(self.current.split('/wiki/')[-1])
             self.current_path.append(self.destination.split('/wiki/')[-1])
             self.current_path = [n.replace('_', ' ').replace('%27', "'") for n in self.current_path]
             self.found = 1
@@ -76,35 +81,84 @@ class PathPuller:
             except RuntimeError:
                 return self.current_path, len(self.visited)
 
+        self.found = -1
         return -1, len(self.visited)
 
 
 
 def status_monitor(pp):
+    fcon = ''
     while not pp.found:
+        start = perf_counter()
+        if pp.con_err:
+            fcon = 'Failed connections: '
+            if len(pp.con_err)<5:
+                for i in pp.con_err[:-1]:
+                    fcon+=f'{i}, ' 
+                fcon+=str(pp.con_err[:-1])
+            else:
+                for i in range(1, 5):
+                    fcon+=f'{pp.con_err[-i]}, '
+                fcon+=pp.con_err[-5]
+                if len(pp.con_err)>5:
+                    fcon+=f', + {len(pp.con_err)} more\n'
+                    
         os.system('cls' if os.name=='nt' else 'clear')
-        print('STATUS MONITOR\n')
-        print(f'Active threads: {threading.active_count()}/{NUM_THREADS}')
-        print(f'Layer: {pp.depth}')
-        print(f'Visited pages: {len(pp.visited)}')
-        print(f'Last seen: {pp.visited[-1] if len(pp.visited)>0 else None}')
-        sleep(1)
+        print(f'''
+        STATUS MONITOR\n\n
+        Active threads: {threading.active_count()}/{NUM_THREADS}\n
+        Layer: {pp.depth}\n
+        Visited pages: {len(pp.visited)}\n
+        Last seen: {pp.visited[-1] if len(pp.visited)>0 else None}\n
+        {fcon}
+        ''', flush=True)
+
+        while perf_counter()-start<1:
+            pass
+    
+    print_path(pp.current_path)   
+    a = 'NOT ' if pp.found==-1 else ''
+    print(f'\n\nPATH {a}FOUND\nTerminating remaining threads...')
+
+
+def print_path(result):
+    if result!=-1:
+        for i in result:
+            if i!=result[0]:
+                print(f' --> {i}', end='')
+            else:
+                print(f'\n{result[0]}', end='')
+    
+    else:
+        print('No connection in specified range')
 
 
 def get_user_input():
     wiki = wikipediaapi.Wikipedia('en')
-    while True:
+
+    json_err_check = page_exist_check = 1
+    while page_exist_check:
         source = input('Source page>> ')
 
         source = source.replace(' ', '_')
         source = f'https://wikipedia.org/wiki/{source}' if not '/wiki/' in source else source
         tocheck = source.split('/wiki/')[-1]
+        while json_err_check:
+            try:
+                if wiki.page(tocheck).exists():
+                    print(f'Setting source page: {source}')
+                    page_exist_check = 0
+                else:
+                    print(f'\nSource page does not exist: {source}\n')     
+                json_err_check = 0
+                
+            except json.decoder.JSONDecodeError:
+                for i in range(10):
+                    os.system('cls' if os.name=='nt' else 'clear')  
+                    print('Request blocked')
+                    print(f'Encountered JSON decoder error, waiting {10-i} seconds...')
+                    sleep(1)
 
-        if wiki.page(tocheck).exists():
-            print(f'Setting source page: {source}')
-            break
-        else:
-            print(f'\nSource page does not exist: {source}\n')
 
     print('\n')
     while True:
@@ -123,19 +177,19 @@ def get_user_input():
     print('\n')
     while True:
         try:
-            range = int(input('Max range>> '))
+            max_range = int(input('Max range>> '))
             break
         except ValueError:
             print(f'\nMust be a number\n')
 
-    return source, dest, range
+    return source, dest, max_range
 
 
 def main():
     os.system('cls' if os.name=='nt' else 'clear')
-    source, dest, range = get_user_input()
+    source, dest, max_range = get_user_input()
 
-    bs = PathPuller(source, dest, max_depth=range)
+    bs = PathPuller(source, dest, max_depth=max_range)
     
     loading_thread = threading.Thread(target=status_monitor, args=[bs], daemon=True)
     loading_thread.start()
@@ -148,15 +202,7 @@ def main():
     os.system('cls' if os.name=='nt' else 'clear')
     print('RESULT\n')
     print(f'{num} page{fin} visited\nTime: {perf_counter()-start_timer:.2f}s')
-    if result!=-1:
-        for i in result:
-            if i!=result[0]:
-                print(f' --> {i}', end='')
-            else:
-                print(f'\n{result[0]}', end='')
-
-    else:
-        print('No connection in specified range')
+    print_path(result)
 
     print('\n')
     input('Press Enter to restart...')
